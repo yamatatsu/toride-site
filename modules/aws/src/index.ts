38a6bea74b3e18833966457e0cad95aa61b326ec
yamatatsu <the.kinnikuman@gmail.com>
2019-04-10 15:15:39 +0900

    fix: lambda

--- modules/aws/src/index.ts
+++ modules/aws/src/index.ts
@@ -14,32 +14,36 @@ export class AwsStack extends cdk.Stack {
       bucketName,
     })
 
-    const builderFunction = new lambda.Function(this, "BuilderFunction", {
-      code: new lambda.AssetCode(__dirname + "/../lambda"),
-      handler: "handler",
-      runtime: lambda.Runtime.NodeJS810,
-      environment: {
-        NETLIFY_BUILD_WEBHOOK_URL,
-      },
-      // timeout?: number,
-      // functionName: string,
-      // memorySize?: number,
-      // initialPolicy?: iam.PolicyStatement[],
-      // role?: iam.IRole,
-      // vpc?: ec2.IVpcNetwork,
-      // vpcSubnets?: ec2.SubnetSelection,
-      // securityGroup?: ec2.ISecurityGroup,
-      // allowAllOutbound?: boolean,
-      // deadLetterQueueEnabled?: boolean,
-      // deadLetterQueue?: sqs.IQueue,
-      // tracing?: Tracing,
-      // layers?: ILayerVersion[],
-      // reservedConcurrentExecutions?: number,
-      // events?: IEventSource[],
-      // logRetentionDays?: logs.RetentionDays,
-    })
+    const lambdaHookNetlifyBuild = new lambda.Function(
+      this,
+      "HookNetlifyBuild",
+      {
+        code: new lambda.AssetCode(__dirname + "/../lambda"),
+        handler: "hookNetlifyBuild.handler",
+        runtime: lambda.Runtime.NodeJS810,
+        environment: {
+          NETLIFY_BUILD_WEBHOOK_URL,
+        },
+        // timeout?: number,
+        // functionName: string,
+        // memorySize?: number,
+        // initialPolicy?: iam.PolicyStatement[],
+        // role?: iam.IRole,
+        // vpc?: ec2.IVpcNetwork,
+        // vpcSubnets?: ec2.SubnetSelection,
+        // securityGroup?: ec2.ISecurityGroup,
+        // allowAllOutbound?: boolean,
+        // deadLetterQueueEnabled?: boolean,
+        // deadLetterQueue?: sqs.IQueue,
+        // tracing?: Tracing,
+        // layers?: ILayerVersion[],
+        // reservedConcurrentExecutions?: number,
+        // events?: IEventSource[],
+        // logRetentionDays?: logs.RetentionDays,
+      }
+    )
 
-    s3Bucket.onEvent(s3.EventType.ObjectCreated, builderFunction, {
+    s3Bucket.onEvent(s3.EventType.ObjectCreated, lambdaHookNetlifyBuild, {
       prefix: "content",
     })
   }
